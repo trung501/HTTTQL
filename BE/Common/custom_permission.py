983ddecb0911bc7964bdd9b7b23e519b54ee30a2
trung501 <gotrungogle@gmail.com>
2022-12-01 08:02:49 +0700

    update

---
+++ BE/Common/custom_permission.py
@@ -0,0 +1,38 @@
+from rest_framework import permissions
+from Common import generics_cursor
+from Common.generics import NO_ROLE
+class CustomPermissions(permissions.BasePermission):
+    def __init__(self):
+        super().__init__()
+
+    # def get_allowed_methods(self, username):
+    #     ## query in database here
+    #     username = username
+    #     return ['GET','POST','PUT','DELETE']
+
+    def get_allowed_methods(self, CODE_VIEW):
+        if int(CODE_VIEW) > NO_ROLE: # role admin
+            return ['GET','POST','PUT','DELETE']
+        else:
+            return []
+    
+    def has_permission(self, request, view):
+        ## check if token is valid
+        if not (request.user and request.user.is_authenticated):
+            return False
+        ## get username
+        username = request.user
+        query_string = "SELECT ROLEID FROM ACCOUNT_USER WHERE USERNAME = %s"
+        param = [str(username)]
+        try:
+            obj = generics_cursor.getDictFromQuery(query_string,param)
+        
+            ## get allowed methods by username [query to database]
+            allowed_methods = self.get_allowed_methods(obj[0].get("CODE_VIEW"))
+            # allowed_methods = self.search_allowed_methods
+            ## check if request method in allowed methods
+            if request.method in allowed_methods :
+                return True
+            return False
+        except:
+            return False
