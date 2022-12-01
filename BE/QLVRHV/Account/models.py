34e1cdb485de142762faf70c847b428bbf3775a3
trung501 <gotrungogle@gmail.com>
2022-12-01 08:05:24 +0700

    update

--- BE/QLVRHV/Account/models.py
+++ BE/QLVRHV/Account/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from Common.generics import NO_ROLE
 # Create your models here.
 class User(AbstractUser):
     username = models.CharField(max_length=255,unique=True)
     password= models.CharField(max_length=255)
     personID= models.CharField(max_length=20, blank=True, null=True)
-    roleID = models.IntegerField(blank=True,default=0, null=True)
+    roleID = models.IntegerField(blank=True,default=NO_ROLE, null=True)
 
     USERNAME_FIELD= 'username'
     REQUIRED_FIELDS=[]
