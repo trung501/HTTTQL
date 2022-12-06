a705d372eb5122ef831901e9990b766a6f3d46a9
trung501 <gotrungogle@gmail.com>
2022-12-06 21:52:53 +0700

    update db

--- BE/QLVRHV/Account/models.py
+++ BE/QLVRHV/Account/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
     password= models.CharField(max_length=255)
     personID= models.CharField(max_length=20, blank=True, null=True)
     roleID = models.IntegerField(blank=True,default=NO_ROLE, null=True)
+    userSetRole = models.CharField(max_length=255,blank=True,null=True)
 
     USERNAME_FIELD= 'username'
     REQUIRED_FIELDS=[]
