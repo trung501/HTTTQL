6de258c67519f50c6fc604b15ffe247ae0054a9f
trung501 <gotrungogle@gmail.com>
2022-12-21 12:04:19 +0700

    update api dang ky ra ngoai

--- BE/QLVRHV/Person/views.py
+++ BE/QLVRHV/Person/views.py
@@ -9,7 +9,9 @@ from rest_framework import status
 from rest_framework import pagination
 from Common import generics_cursor, custom_permission
 from Common.generics import *
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
+
+
 class PersonPermission(custom_permission.CustomPermissions):
     def __init__(self):
         super().__init__()
@@ -25,23 +27,23 @@ class PersonViewSet(viewsets.ViewSet):
     """
     Interact with UserCam
     """
-    
+
     permission_classes = [PersonPermission]
 
-    # all swagger's parameters should be defined here 
+    # all swagger's parameters should be defined here
     sw_page = openapi.Parameter(
         name='page', type=openapi.TYPE_STRING, description="Page number", in_=openapi.IN_QUERY)
     sw_size = openapi.Parameter(
         name='size', type=openapi.TYPE_STRING, description="Number of results to return per page", in_=openapi.IN_QUERY)
     sw_DonViID = openapi.Parameter(
-        name='donViID', type=openapi.TYPE_STRING, description="DonViID",default="DD155", in_=openapi.IN_QUERY)
+        name='donViID', type=openapi.TYPE_STRING, description="Mã đơn vị ( lớp, đại đội, tiểu đoàn)", default="DD155", in_=openapi.IN_QUERY)
     sw_MaHV = openapi.Parameter(
-        name='maHV', type=openapi.TYPE_STRING, description="MaHV",default="201901058", in_=openapi.IN_QUERY)
+        name='maHV', type=openapi.TYPE_STRING, description="MaHV", default="201901058", in_=openapi.IN_QUERY)
     sw_TimeStart = openapi.Parameter(
-        name='timeStart', type=openapi.TYPE_STRING, description="TimeStart",default="12-08-2022", in_=openapi.IN_QUERY)
+        name='timeStart', type=openapi.TYPE_STRING, description="Thời gian đi", default="12-08-2022", in_=openapi.IN_QUERY)
     sw_TimeBetween = openapi.Parameter(
-        name='timeBetween', type=openapi.TYPE_STRING, description="TimeBetween",default="12-08-2022", in_=openapi.IN_QUERY)
-    
+        name='timeBetween', type=openapi.TYPE_STRING, description="Time trong tuần", default="12-08-2022", in_=openapi.IN_QUERY)
+
     get_list_person_response = {
         status.HTTP_500_INTERNAL_SERVER_ERROR: 'INTERNAL_SERVER_ERROR',
         status.HTTP_204_NO_CONTENT: 'NO_CONTENT',
@@ -54,44 +56,45 @@ class PersonViewSet(viewsets.ViewSet):
         status.HTTP_200_OK: 'JSON',
     }
 
-    def CheckQuyetDinhCamTrai(self,maHV,time_go:"12-08-2022"):
+    def CheckQuyetDinhCamTrai(self, maHV, time_go: "12-08-2022"):
         try:
             time_go = datetime.strptime(time_go, '%d-%m-%Y').date()
-            query_string=f'SELECT * FROM QUYETDINHCAMTRAI \
+            query_string = f'SELECT * FROM QUYETDINHCAMTRAI \
                             WHERE MAHV = %s \
                             AND TG_BatDau <= %s\
-                            AND TG_BatDau <= %s  '            
-            obj=generics_cursor.getDictFromQuery(query_string,[maHV,time_go,time_go])
-            if len(obj) > 0 :
-                return True,obj
+                            AND TG_BatDau <= %s  '
+            obj = generics_cursor.getDictFromQuery(
+                query_string, [maHV, time_go, time_go])
+            if len(obj) > 0:
+                return True, obj
         except:
-            return True,[]
-        return False,[]
-        
-    def getTimeStartAndFinishWeek(self,time_beetween:"12-08-2022"):
+            return True, []
+        return False, []
+
+    def getTimeStartAndFinishWeek(self, time_beetween: "12-08-2022"):
         try:
             dt = datetime.strptime(time_beetween, '%d-%m-%Y')
             start = dt - timedelta(days=dt.weekday())
             end = start + timedelta(days=6)
-            return start.strftime('%Y-%m-%d'),end.strftime('%Y-%m-%d')
+            return start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')
         except:
-            return None,None
+            return None, None
 
-
-    @swagger_auto_schema(method='get', manual_parameters=[sw_page,sw_size,sw_DonViID], responses=get_list_person_response)
+    @swagger_auto_schema(method='get', manual_parameters=[sw_page, sw_size, sw_DonViID], responses=get_list_person_response)
     @action(methods=['GET'], detail=False, url_path='get-list-hoc-vien')
     def get_list_hoc_vien(self, request):
         """
         API này dùng để lấy danh sách học viên của một đơn vị cụ thể nào, có thể là lớp,đại đội, tiểu đoàn. Để sử dụng phân trang thì nhập thêm param page, size vào.
         """
-        page =request.query_params.get('page')
-        size =request.query_params.get('size')
-        donViID =str(request.query_params.get('donViID'))
+        page = request.query_params.get('page')
+        size = request.query_params.get('size')
+        donViID = str(request.query_params.get('donViID'))
         try:
-            query_string="SELECT * FROM HOCVIEN \
+            query_string = "SELECT * FROM HOCVIEN \
                             INNER JOIN PERSON ON HOCVIEN.PERSONID = PERSON.PersonID\
-                            WHERE PERSON.DonViID IN (SELECT DonViID FROM DONVI WHERE DONVI.MaLop = %s OR DONVI.MaDaiDoi= %s OR DONVI.MaTieuDoan =%s)"            
-            obj=generics_cursor.getDictFromQuery(query_string,[donViID,donViID,donViID],page=page,size=size)
+                            WHERE PERSON.DonViID IN (SELECT DonViID FROM DONVI WHERE DONVI.MaLop = %s OR DONVI.MaDaiDoi= %s OR DONVI.MaTieuDoan =%s)"
+            obj = generics_cursor.getDictFromQuery(
+                query_string, [donViID, donViID, donViID], page=page, size=size)
             if obj is None:
                 return Response(data={}, status=status.HTTP_204_NO_CONTENT)
         except:
@@ -104,76 +107,90 @@ class PersonViewSet(viewsets.ViewSet):
         """
         API này dùng để lấy quyền truy cập ( roleID) của user hiện tại).
         """
-        roleId = request.user.roleID       
-        return Response({"permission":int(roleId)}, status=status.HTTP_200_OK)
+        roleId = request.user.roleID
+        return Response({"permission": int(roleId)}, status=status.HTTP_200_OK)
 
-    @swagger_auto_schema(method='get', manual_parameters=[sw_MaHV,sw_TimeStart], responses=get_list_person_response)
+    @swagger_auto_schema(method='get', manual_parameters=[sw_MaHV, sw_TimeStart], responses=get_list_person_response)
     @action(methods=['GET'], detail=False, url_path='get-check-cam-trai')
     def get_check_cam_trai(self, request):
         """
         API này dùng để check thử xem học viên có bị cấm trại trong khoảng thời gian đăng ký ra ngoài không, tham số nhập vào là mã học viên và thời gian đăng ký ra ngoài.
         """
-        maHV =request.query_params.get('maHV')
-        timeStart = request.query_params.get('timeStart')       
+        maHV = request.query_params.get('maHV')
+        timeStart = request.query_params.get('timeStart')
 
-        checkCamTrai,listReason = self.CheckQuyetDinhCamTrai(maHV,timeStart)
+        checkCamTrai, listReason = self.CheckQuyetDinhCamTrai(maHV, timeStart)
         if checkCamTrai:
-            if len(listReason)==0:
-                return Response(data={"result":True}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            return Response(data={"result":True,"reason":listReason}, status=status.HTTP_200_OK)
-      
-        return Response(data={"result":False}, status=status.HTTP_200_OK)
-    
-    @swagger_auto_schema(method='get', manual_parameters=[sw_DonViID,sw_TimeBetween], responses=get_list_person_response)
+            if len(listReason) == 0:
+                return Response(data={"result": True}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(data={"result": True, "reason": listReason}, status=status.HTTP_200_OK)
+
+        return Response(data={"result": False}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(method='get', manual_parameters=[sw_DonViID, sw_TimeBetween], responses=get_list_person_response)
     @action(methods=['GET'], detail=False, url_path='get-list-cam-trai-in-week')
     def get_list_cam_trai_in_week(self, request):
         """
         API này dùng để lấy danh sách các học viên bị cắm trại trong một đơn vị nào đó. timeBetween là lựa chọn, nếu không nhập sẽ lấy thời gian ngày hôm nay. API sẽ tìm tất cả các học viên bị cấm trại từ đầu tuần đến cuối tuần nằm trong timeBetween đó.
         """
-        donViID =request.query_params.get('donViID')
-        timeBetween = request.query_params.get('timeBetween')       
-        page = request.query_params.get('page')       
-        size = request.query_params.get('size')       
+        donViID = request.query_params.get('donViID')
+        timeBetween = request.query_params.get('timeBetween')
+        page = request.query_params.get('page')
+        size = request.query_params.get('size')
         if timeBetween is None:
-            timeBetween=datetime.now().strftime("%d-%m-%Y")
-        time_start,time_end = self.getTimeStartAndFinishWeek(timeBetween)
-        print(time_start,time_end)
+            timeBetween = datetime.now().strftime("%d-%m-%Y")
+        time_start, time_end = self.getTimeStartAndFinishWeek(timeBetween)
+        print(time_start, time_end)
         try:
-            query_string=f"SELECT * FROM QUYETDINHCAMTRAI WHERE \
+            query_string = f"SELECT * FROM QUYETDINHCAMTRAI WHERE \
                             ((TG_BatDau BETWEEN '{time_start}'AND '{time_end}') OR \
                             (TG_KetThuc BETWEEN '{time_start}'AND '{time_end}') OR  \
                             (TG_BatDau <= '{time_start}' AND TG_KetThuc >= '{time_end}'))\
                             AND MAHV IN (SELECT MAHV FROM HOCVIEN,PERSON,DONVI WHERE HOCVIEN.personID = PERSON.PersonID AND DONVI.DonViID=PERSON.DonViID\
-                            AND (DONVI.MaLop = %s OR DONVI.MaDaiDoi= %s OR DONVI.MaTieuDoan =%s))" 
-            obj=generics_cursor.getDictFromQuery(query_string,[donViID,donViID,donViID],page=page,size=size)
+                            AND (DONVI.MaLop = %s OR DONVI.MaDaiDoi= %s OR DONVI.MaTieuDoan =%s))"
+            obj = generics_cursor.getDictFromQuery(
+                query_string, [donViID, donViID, donViID], page=page, size=size)
             if obj is None:
                 return Response(data={}, status=status.HTTP_204_NO_CONTENT)
         except:
             return Response(data={}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(data=obj, status=status.HTTP_200_OK)
 
-    # @swagger_auto_schema(method='post', manual_parameters=[sw_DonViID,sw_TimeBetween], responses=post_list_person_response)
-    # @action(methods=['POST'], detail=False, url_path='post-dang-ky-ra-ngoai')
-    # def post_dang_ky_ra_ngoai(self, request):
-    #     donViID =request.query_params.get('donViID')
-    #     timeBetween = request.query_params.get('timeBetween')       
-    #     page = request.query_params.get('page')       
-    #     size = request.query_params.get('size')       
-    #     if timeBetween is None:
-    #         timeBetween=datetime.now().strftime("%d-%m-%Y")
-    #     time_start,time_end = self.getTimeStartAndFinishWeek(timeBetween)
-    #     print(time_start,time_end)
-    #     try:
-    #         query_string=f"SELECT * FROM QUYETDINHCAMTRAI WHERE \
-    #                         ((TG_BatDau BETWEEN '{time_start}'AND '{time_end}') OR \
-    #                         (TG_KetThuc BETWEEN '{time_start}'AND '{time_end}') OR  \
-    #                         (TG_BatDau <= '{time_start}' AND TG_KetThuc >= '{time_end}'))\
-    #                         AND MAHV IN (SELECT MAHV FROM HOCVIEN,PERSON,DONVI WHERE HOCVIEN.personID = PERSON.PersonID AND DONVI.DonViID=PERSON.DonViID\
-    #                         AND (DONVI.MaLop = %s OR DONVI.MaDaiDoi= %s OR DONVI.MaTieuDoan =%s))" 
-    #         obj=generics_cursor.getDictFromQuery(query_string,[donViID,donViID,donViID],page=page,size=size)
-    #         if obj is None:
-    #             return Response(data={}, status=status.HTTP_204_NO_CONTENT)
-    #     except:
-    #         return Response(data={}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    #     return Response(data=obj, status=status.HTTP_200_OK)
-
+    @swagger_auto_schema(method='post', manual_parameters=[], request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT, required=None,
+        properties={
+            'hinh_thuc_RN': openapi.Schema(type=openapi.TYPE_INTEGER, description="0 là tranh thủ,1 là ra ngoài", default=1),
+            'dia_diem': openapi.Schema(type=openapi.TYPE_STRING, description="Địa điểm", default="Hà Nội"),
+            'time_start': openapi.Schema(type=openapi.TYPE_STRING, default='2022-11-11 16:30'),
+            'time_end': openapi.Schema(type=openapi.TYPE_STRING, default='2022-11-13 18:00'),
+            'ma_HV': openapi.Schema(type=openapi.TYPE_STRING, default='202104043'),
+        }
+    ), responses=post_list_person_response)
+    @action(methods=['POST'], detail=False, url_path='post-dang-ky-ra-ngoai')
+    def post_dang_ky_ra_ngoai(self, request):
+        """
+        API này dùng để đăng ký học viên ra ngoài. Mặc định trái thái xét duyệt sẽ là 0. Đối với hình thức ra ngoài, nhập 0 nếu là tranh thủ, nhập 1 nếu là ra ngoài. 
+        """
+        dataDict = request.data
+        try:
+            hinhThucRN = int(dataDict.get("hinh_thuc_RN"))
+            if hinhThucRN == 0:
+                hinhThucRN = "Tranh thủ"
+            else:
+                hinhThucRN = "Ra ngoài"
+            diaDiem = dataDict.get("dia_diem")
+            maHV = dataDict.get("ma_HV")
+            timeStart = dataDict.get("time_start")
+            timeEnd = dataDict.get("time_end")
+            timeStart = datetime.strptime(timeStart, '%Y-%m-%d %H:%M')
+            timeEnd = datetime.strptime(timeEnd, '%Y-%m-%d %H:%M')
+            query_string = f'INSERT INTO DSDANGKY("HinhThucRN","DiaDiem","ThoiGianDi","ThoiGianVe","MaHV","TRANGTHAIXD") VALUES (%s,%s,%s,%s,%s,0);'
+            param = [hinhThucRN, diaDiem, timeStart, timeEnd, maHV]
+            with connection.cursor() as cursor:
+                cursor.execute(query_string, param)
+                rows_affected = cursor.rowcount
+            if rows_affected == 0:
+                return Response(data={"status": False}, status=status.HTTP_200_OK)
+        except:
+            return Response(data={}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(data={"status": True}, status=status.HTTP_200_OK)
